c32f2c27fd6d37ccba2a63834f8977a08373a971
chethwijesinghe <chethzz@hotmail.com>
2024-04-24 18:10:31 +1000

    Implement Generic repository.

--- FitnessPlace.DataAccess/Repositories/GenericRepository.cs
+++ FitnessPlace.DataAccess/Repositories/GenericRepository.cs
@@ -1,12 +1,21 @@
 using FitnessPlace.DataAccess.Interfaces;
+using Microsoft.EntityFrameworkCore;
 
 namespace FitnessPlace.DataAccess.Repositories
 {
     public class GenericRepository<T> : IGenericRepository<T> where T : class
     {
-        public Task<List<T?>> GetAsync(bool tracked = true)
+        private readonly FitnessPlaceDbContext _context;
+        private readonly DbSet<T> _dbSet;
+
+        public async Task<List<T?>> GetAsync(bool tracked = true)
         {
-            throw new NotImplementedException();
+            IQueryable<T> query = _dbSet;
+            if (!tracked)
+            {
+                query = query.AsNoTracking();
+            }
+            return await query.ToListAsync();
         }
     }
 }
