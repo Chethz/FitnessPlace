f1e24eadeaedfc399bed528cf20b9b0a2c1ef3b1
chethwijesinghe <chethzz@hotmail.com>
2024-04-24 16:46:33 +1000

    Added models

---
+++ FitnessPlace.DataAccess/Models/Member.cs
@@ -0,0 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace FitnessPlace.DataAccess.Models{
+        public class Member
+{
+    [Key]
+    public int Id { get; set; }
+    [Required]
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public MemberDetails MemberDetails { get; set; }
+}
+}
