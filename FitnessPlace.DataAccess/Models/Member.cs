7a9a72f486f983543e3d7485ef7cadf54cf2c0c6
chethwijesinghe <chethzz@hotmail.com>
2024-04-24 16:54:52 +1000

    Edit reference

--- FitnessPlace.DataAccess/Models/Member.cs
+++ FitnessPlace.DataAccess/Models/Member.cs
@@ -1,13 +1,14 @@
 using System.ComponentModel.DataAnnotations;
 
-namespace FitnessPlace.DataAccess.Models{
-        public class Member
+namespace FitnessPlace.DataAccess.Models
 {
-    [Key]
-    public int Id { get; set; }
-    [Required]
-    public string FirstName { get; set; }
-    public string LastName { get; set; }
-    public MemberDetails MemberDetails { get; set; }
-}
+    public class Member
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public MemberDetail MemberDetail { get; set; }
+    }
 }
