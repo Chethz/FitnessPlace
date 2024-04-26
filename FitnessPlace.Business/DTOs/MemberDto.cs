15167e0b52ee62b3666ccdb1acdcc7c3f17731ce
chethwijesinghe <chethzz@hotmail.com>
2024-04-26 17:47:17 +1000

    Removed unnessosary annotations

--- FitnessPlace.Business/DTOs/MemberDto.cs
+++ FitnessPlace.Business/DTOs/MemberDto.cs
@@ -1,12 +1,8 @@
-using System.ComponentModel.DataAnnotations;
-
 namespace FitnessPlace.Business.DTOs
 {
     public class MemberDto
     {
-        [Key]
         public int Id { get; set; }
-        [Required]
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public MemberDetailDto MemberDetail { get; set; }
