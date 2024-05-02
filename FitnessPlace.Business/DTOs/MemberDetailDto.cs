be6744378226b885ffb519a51e719c88edee026f
chethwijesinghe <chethzz@hotmail.com>
2024-05-03 08:51:49 +1000

    Remove unnessosary properties

--- FitnessPlace.Business/DTOs/MemberDetailDto.cs
+++ FitnessPlace.Business/DTOs/MemberDetailDto.cs
@@ -2,11 +2,9 @@ namespace FitnessPlace.Business.DTOs
 {
     public class MemberDetailDto
     {
-        public int Id { get; set; }
         public string Email { get; set; }
         public string Address { get; set; }
         public string MobileNumber { get; set; }
         public int MemberId { get; set; }
-        public MemberDto Member { get; set; }
     }
 }
