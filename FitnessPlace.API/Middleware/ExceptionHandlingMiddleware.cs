ef2b3d63c89695d6cbb68222b6c371932f6422bc
chethwijesinghe <chethzz@hotmail.com>
2024-05-06 17:31:37 +1000

    Implemented logging

--- FitnessPlace.API/Middleware/ExceptionHandlingMiddleware.cs
+++ FitnessPlace.API/Middleware/ExceptionHandlingMiddleware.cs
@@ -1,11 +1,14 @@
 using System.Net;
 using FitnessPlace.Business.Exceptions;
 using Microsoft.Data.SqlClient;
+using Serilog;
 
 namespace FitnessPlace.API.Middleware
 {
-    public class ExceptionHandlingMiddleware : IMiddleware
+    public class ExceptionHandlingMiddleware(ILogger<ExceptionHandlingMiddleware> logger) : IMiddleware
     {
+        private readonly ILogger<ExceptionHandlingMiddleware> _logger = logger;
+
         public async Task InvokeAsync(HttpContext context, RequestDelegate next)
         {
             try
@@ -14,11 +17,13 @@ namespace FitnessPlace.API.Middleware
             }
             catch (EntityNotFoundException ex)
             {
+                _logger.LogError(ex, ex.Message);
                 context.Response.StatusCode = (int)HttpStatusCode.NotFound;
                 await context.Response.WriteAsync(ex.Message);
             }
             catch (Exception ex)
             {
+                _logger.LogError(ex, ex.Message);
                 context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
                 await context.Response.WriteAsync(ex.Message);
             }
