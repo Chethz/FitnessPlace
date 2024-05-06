39538962e2a194416a1d36b83c8a1f0fe04a0b4b
chethwijesinghe <chethzz@hotmail.com>
2024-05-06 17:31:13 +1000

    Installed Serilog and Seq for structured logging

--- FitnessPlace.API/Program.cs
+++ FitnessPlace.API/Program.cs
@@ -7,6 +7,7 @@ using FitnessPlace.DataAccess;
 using FitnessPlace.DataAccess.Interfaces;
 using FitnessPlace.DataAccess.Repositories;
 using Microsoft.EntityFrameworkCore;
+using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -23,6 +24,18 @@ builder.Services.AddDbContext<FitnessPlaceDbContext>(
                 options.UseSqlServer(
                     builder.Configuration.GetConnectionString("DefaultConnection"),
                     x => x.MigrationsAssembly("FitnessPlace.DataAccess")));
+
+//Add support to logging with SERILOG
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Information()
+    .WriteTo.Console()
+    .WriteTo.File("/Logs/log.txt", rollingInterval: RollingInterval.Day)
+    .WriteTo.Seq("http://localhost:5341")
+    .CreateLogger();
+
+Log.Information("Applictaion Starting");
+
+builder.Host.UseSerilog(Log.Logger);
 
 //Adding automapper
 builder.Services.AddAutoMapper(typeof(MappingProfile));
