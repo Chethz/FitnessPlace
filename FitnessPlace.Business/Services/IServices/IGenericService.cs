c66aa28c6a537bd11c6e70c502d14de6783a6c3b
chethwijesinghe <chethzz@hotmail.com>
2024-05-10 16:14:22 +1000

    Removed tracking.

--- FitnessPlace.Business/Services/IServices/IGenericService.cs
+++ FitnessPlace.Business/Services/IServices/IGenericService.cs
@@ -3,7 +3,7 @@ namespace FitnessPlace.Business.Services.IServices
     public interface IGenericService<TEntity, TDto> where TEntity : class where TDto : class
     {
         Task<IEnumerable<TDto>> GetAsync(bool tracked = true);
-        Task<TDto?> GetByIdAsync(int id, bool tracked = true);
+        Task<TDto?> GetByIdAsync(int id);
         Task AddAsync(TEntity entity);
         Task UpdateAsync(int id, TEntity entity);
         Task DeleteByIdAsync(int id);
