a4955f844635799a735ce70bc1474edba12c1f9e
chethwijesinghe <chethzz@hotmail.com>
2024-05-10 16:07:57 +1000

    New Dto for create member

---
+++ FitnessPlace.Business/DTOs/MemberCreateDto.cs
@@ -0,0 +1,11 @@
+namespace FitnessPlace.Business.DTOs
+{
+    public class MemberCreateDto
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+        public string MobileNumber { get; set; }
+    }
+}
