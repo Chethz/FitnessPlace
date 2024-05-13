5c3919162a4ee59987977a0cb1f9de47bdf07978
chethz <chethiya.w@gmail.com>
2024-05-13 12:46:19 +1000

    Added validations.

--- FitnessPlace.Business/DTOs/MemberDetailDto.cs
+++ FitnessPlace.Business/DTOs/MemberDetailDto.cs
@@ -1,7 +1,10 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace FitnessPlace.Business.DTOs
 {
     public class MemberDetailDto
     {
+        [Required]
         public string Email { get; set; }
         public string Address { get; set; }
         public string MobileNumber { get; set; }
--- FitnessPlace.Business/DTOs/MemberDto.cs
+++ FitnessPlace.Business/DTOs/MemberDto.cs
@@ -1,8 +1,11 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace FitnessPlace.Business.DTOs
 {
     public class MemberDto
     {
         public int Id { get; set; }
+        [Required]
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public MemberDetailDto MemberDetail { get; set; }
