7761bc329023cd3e5c1145dacb44e427d6e072c1
chethwijesinghe <chethzz@hotmail.com>
2024-05-27 18:05:12 +1000

    Added Unit tests

---
+++ FitnessPlace.API.IntegrationTests/FitnessPlaceControllerTests.cs
@@ -0,0 +1,18 @@
+﻿using System.Net;
+
+namespace FitnessPlace.API.IntegrationTests;
+
+public class FitnessPlaceControllerTests
+{
+    //[ThingsUnderTest]_Should_[ExpectedResult]
+    [Fact]
+    public async Task Test1()
+    {
+        var application = new FitnessPlaceWebApplicationFactory();
+        var client = application.CreateClient();
+
+        var response = await client.GetAsync("/api/Member");
+
+        Assert.Equal(HttpStatusCode.NotFound, response.StatusCode);
+    }
+}
