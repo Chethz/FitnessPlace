6b4a3ae7468094e51e5019c4f6ade5cb52d558d8
chethz <chethiya.w@gmail.com>
2024-05-28 15:48:28 +1000

    Added tests

--- FitnessPlace.API.IntegrationTests/FitnessPlaceControllerTests.cs
+++ FitnessPlace.API.IntegrationTests/FitnessPlaceControllerTests.cs
@@ -1,4 +1,6 @@
 ﻿using System.Net;
+using System.Net.Http.Json;
+using FitnessPlace.Business.DTOs;
 
 namespace FitnessPlace.API.IntegrationTests;
 
@@ -6,7 +8,7 @@ public class FitnessPlaceControllerTests
 {
     //[ThingsUnderTest]_Should_[ExpectedResult]
     [Fact]
-    public async Task Test1()
+    public async Task WhenNoMembers_Should_ReturnNotFound()
     {
         var application = new FitnessPlaceWebApplicationFactory();
         var client = application.CreateClient();
@@ -14,5 +16,24 @@ public class FitnessPlaceControllerTests
         var response = await client.GetAsync("/api/Member");
 
         Assert.Equal(HttpStatusCode.NotFound, response.StatusCode);
+    }
+
+    [Fact]
+    public async Task CreateMembers_Should_ReturnCreated()
+    {
+        var application = new FitnessPlaceWebApplicationFactory();
+        var client = application.CreateClient();
+        MemberCreateDto memberCreateDto = new()
+        {
+            FirstName = "Sam",
+            LastName = "Jenkins",
+            Address = "This is test address",
+            Email = "testemail@mail.com",
+            MobileNumber = "0466822133"
+        };
+        JsonContent MemberCreateDtoContent = JsonContent.Create(memberCreateDto);
+        var response = await client.PostAsync("/api/Member", MemberCreateDtoContent);
+
+        Assert.Equal(HttpStatusCode.Created, response.StatusCode);
     }
 }
